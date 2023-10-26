9a0c81ab05357183daa0bdbd9a2df67c8af38e1e
Kazuaki KADOYA <kadoya@penpale.jp>
2023-10-26 15:51:16 +0900

    Update Corrector.php
    ファイル名だけ取得追加

--- Corrector.php
+++ Corrector.php
@@ -2,13 +2,20 @@
 
 namespace Acms\Custom;
 
-/**
- * 拡張子だけ取得
- */
 class Corrector
 {
+    /**
+     * 拡張子だけ取得
+     */
     public static function getext($txt, $args = array()) {
         $extention = pathinfo($txt, PATHINFO_EXTENSION);
+        return $extention;
+    }
+    /**
+     * ファイル名だけ取得
+     */
+    public static function getfilename($txt, $args = array()) {
+        $extention = pathinfo($txt, PATHINFO_FILENAME);
         return $extention;
     }
 }
